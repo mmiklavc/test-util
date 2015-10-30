e229343de4f6d6b5b2dac2375da50cc8b9264489
Michael Miklavcic <mmiklavcic@hortonworks.com>
2015-10-30 16:17:51 -0400

    Initial commit

---
+++ src/main/java/com/michaelmiklavcic/test/util/TestUtils.java
@@ -0,0 +1,139 @@
+package com.michaelmiklavcic.test.util;
+
+import static org.hamcrest.Matchers.instanceOf;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+import java.io.*;
+import java.lang.reflect.Method;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Properties;
+
+public class TestUtils {
+
+    public static void write(File file, String[] contents) throws IOException {
+        StringBuilder b = new StringBuilder();
+        for (String line : contents) {
+            b.append(line);
+            b.append(System.lineSeparator());
+        }
+        write(file, b.toString());
+    }
+
+    /**
+     * Returns file passed in after writing
+     * 
+     * @param file
+     * @param contents
+     * @return
+     * @throws IOException
+     */
+    public static File write(File file, String contents) throws IOException {
+        com.google.common.io.Files.createParentDirs(file);
+        com.google.common.io.Files.write(contents, file, StandardCharsets.UTF_8);
+        return file;
+    }
+
+    /**
+     * Load properties from string into properties object
+     */
+    public static Properties loadProperties(String contents) throws IOException {
+        Properties props = new Properties();
+        try (ByteArrayInputStream is = new ByteArrayInputStream(contents.getBytes(StandardCharsets.UTF_8))) {
+            props.load(is);
+        }
+        return props;
+    }
+
+    /**
+     * By default cleans up after test run via runtime shutdown hooks
+     */
+    public static File createTempDir(String prefix) throws IOException {
+        return createTempDir(prefix, true);
+    }
+
+    /**
+     * Passing cleanup=true cleans up after test run via runtime shutdown hooks
+     */
+    public static File createTempDir(String prefix, boolean cleanup) throws IOException {
+        final Path tmpDir = Files.createTempDirectory(prefix);
+        if (cleanup) {
+            Runtime.getRuntime().addShutdownHook(new Thread() {
+                @Override
+                public void run() {
+                    try {
+                        cleanDir(tmpDir);
+                    } catch (IOException e) {
+                        System.out.println("Warning: Unable to clean tmp folder.");
+                    }
+                }
+
+            });
+        }
+        return tmpDir.toFile();
+    }
+
+    public static void cleanDir(Path dir) throws IOException {
+        Files.walkFileTree(dir, new SimpleFileVisitor<Path>() {
+
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                Files.delete(file);
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+                Files.delete(file);
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                if (exc == null) {
+                    return FileVisitResult.CONTINUE;
+                } else {
+                    throw exc;
+                }
+            }
+        });
+    }
+
+    public static void assertException(Class<? extends Throwable> exceptionType, Class<?> clazz, String methodName, TypedVal... typedVals)
+            throws Exception {
+        assertException(exceptionType, clazz, methodName, false, typedVals);
+    }
+
+    public static void assertException(Class<? extends Throwable> exceptionType, Class<?> clazz, String methodName, boolean verbose,
+            TypedVal... typedVals) throws Exception {
+        // rawtypes: we're dealing with Objects at this level
+        @SuppressWarnings("rawtypes")
+        Class[] types = new Class[typedVals.length];
+        Object[] args = new Object[typedVals.length];
+        int i = 0;
+        for (TypedVal arg : typedVals) {
+            types[i] = arg.type;
+            args[i] = arg.val;
+            i++;
+        }
+        Object obj = null;
+        try {
+            obj = clazz.newInstance();
+        } catch (InstantiationException ie) {
+            // might be a static method we're trying to call
+        }
+        Method method = clazz.getMethod(methodName, types);
+        try {
+            method.invoke(obj, args);
+            fail("Expected exception of type " + exceptionType);
+        } catch (Throwable t) {
+            if (verbose) {
+                t.printStackTrace();
+            }
+            assertThat("Exception type did not match", t.getCause(), instanceOf(exceptionType));
+        }
+    }
+
+}
